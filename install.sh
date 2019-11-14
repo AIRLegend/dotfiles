a4a60d65d0cba7cd3fddab96ce5f23b1bcc7c804
AIRLegend <alvaroibrain@hotmail.com>
2019-11-14 08:49:54 +0100

    Tmux resurrect

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ ln -s ${DIRECTORY}/.vim ~/.vim
 ln -s ${DIRECTORY}/.vimrc ~/.vimrc
 ln -s ${DIRECTORY}/.zshrc ~/.zshrc
 ln -s ${DIRECTORY}/.tmux.conf ~/.tmux.conf
+ln -s ${DIRECTORY}/.tmux ~/.tmux
 ln -s ${DIRECTORY}/es.utf-8.spl ~/.vim/spell/es.utf-8.spl
 
 #Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+#Install tmux Plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
