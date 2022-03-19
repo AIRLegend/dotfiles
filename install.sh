cc6c32bd1fc67e17eba03dc00befbc1aca8d8cee
alvaroir <air@Alvaros-MacBook-Pro.local>
2022-03-19 16:34:19 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -5,24 +5,25 @@ ln -s ${DIRECTORY}/.vim ~/.vim
 ln -s ${DIRECTORY}/.vimrc ~/.vimrc
 ln -s ${DIRECTORY}/.zshrc ~/.zshrc
 ln -s ${DIRECTORY}/.tmux.conf ~/.tmux.conf
-ln -s ${DIRECTORY}/.tmux ~/.tmux
 ln -s ${DIRECTORY}/es.utf-8.spl ~/.vim/spell/es.utf-8.spl
 
-#Install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-
-#Install tmux Plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+mkdir -p .tmux/plugins/tpm
 
 # Install fzf and other binaries
 if [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
-        brew install fzf
+        brew install tmux fzf vim
         # To install useful key bindings and fuzzy completion:
         $(brew --prefix)/opt/fzf/install
 else
         # Linux
         echo "You'll have to manually install fzf here..."
         echo "You'll have to manually install the_silver_searcher here..."
+fi
 
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+#Install tmux Plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
